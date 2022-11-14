b5a895b5db13373374b4bf625121ac358c0da2f8
azizmenzli <azizmenzli3@gmail.com>
2022-11-14 11:24:49 +0100

    expo folders

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
+import * as React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>RoostIt</Text>
     </View>
+  );
+}
+export default function App() {
+  const Stack = createNativeStackNavigator();
+
+
+  return (
+    <NavigationContainer style={styles.container}>
+    <Stack.Navigator>
+    <Stack.Screen name="home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
